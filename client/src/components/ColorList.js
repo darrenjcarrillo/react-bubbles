2ae04d3cf8e305c81f78f323ccd64a2629f2b964
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-10-18 08:53:21 -0700

    done with login and get req

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -7,7 +8,7 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  console.log(`THIS IS COLORS `, colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -21,6 +22,11 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/api/colors/:id`, colorToEdit)
+      .then(res => {
+        console.log(res);
+      });
   };
 
   const deleteColor = color => {
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,53 @@
-import React from "react";
-
-const Login = () => {
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+const Login = props => {
+  console.log(`THIS IS PROPS LOGIN`, props);
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [user, setUser] = useState({
+    credentials: { userName: "", password: "" }
+  });
+
+  const handleChange = e => {
+    setUser({
+      credentials: {
+        ...user.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  const login = e => {
+    e.preventDefault();
+
+    axiosWithAuth()
+      .post("/api/login", user.credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/protected");
+      })
+      .catch(err => console.log(err.response));
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={user.credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={user.credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log In</button>
+      </form>
     </>
   );
 };
